4f71929348eac643d7c9666f447480fb6bacdb9a
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2024-09-27 14:07:01 +0530

    footer pages desing and added some features

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+
+function page() {
+  return (
+    <div>Hanji kaise hai ap</div>
+  )
+}
+
+export default page
---
+++ src/app/courses/page.tsx
@@ -0,0 +1,7 @@
+function page() {
+  return (
+    <div>Oh to ap courses par hai</div>
+  )
+}
+
+export default page
